f5b22059e444c19c6b4228d7aaafd78eafa0345f
Mundino Hamure <mundinoabete@gmail.com>
2024-11-14 21:27:20 +0000

    collect last 5 market data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -84,6 +85,23 @@ def calculate_surplus_data(sales_row):
 
 
 
+def get_last_5_entries_sales():
+    """
+    Collect last 5 entries of columns of sales worksheet data to make lists of list
+    """
+    sales = SHEET.worksheet('sales')
+
+    columns=[]
+
+    for ind in range(1 , 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+  
+    return columns
+   
+
+
+sales_columns = get_last_5_entries_sales()
 
 
 def main():
@@ -96,5 +114,5 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
     
-main()
+# main()
 
