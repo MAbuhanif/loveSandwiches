8aae9c4d2439525359a55f954d92a77de10fd387
Mundino Hamure <mundinoabete@gmail.com>
2024-11-14 12:43:53 +0000

    refactor update functions in to one

--- run.py
+++ run.py
@@ -49,27 +49,17 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
+
+def update_worksheet(data, worksheet):
     """
-    updating sales worksheet. Add a new row with a new data.
+    update sales and surplus worksheet data
     """
-    print('updating sales worksheet...\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-
-    print('sales worksheet updated succesfully\n')
-
+    print(f"updating {worksheet} worksheet data..")
+    data_worksheet = SHEET.worksheet(worksheet)
+    data_worksheet.append_row(data)
+    print(f"{worksheet} worksheet updated successfuly")
 
 
-def update_surplus_worksheet(data):
-    """
-    updating surplus worksheet. Add a new row with a new data.
-    """
-    print('updating surplus worksheet...\n')
-    surplus_worksheet = SHEET.worksheet('surplus')
-    surplus_worksheet.append_row(data)
-
-    print('surplus worksheet updated succesfully\n')
 
 
 
@@ -102,11 +92,9 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, 'surplus')
     
-
-
 main()
 
