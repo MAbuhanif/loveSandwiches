d4b1b028ba5f4309a069b4a92d53291253b4fef9
Mundino Hamure <mundinoabete@gmail.com>
2024-11-15 10:21:46 +0000

    calculate stock data and adding the value to stock worksheet

--- run.py
+++ run.py
@@ -99,9 +99,22 @@ def get_last_5_entries_sales():
   
     return columns
    
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item and adding 10%
+    """
+    print("calculating the stock data... \n")
+
+    new_stock_data=[]
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column)/len(int_column)
+        ten_percent = average * 0.1
+        stock_sum = average + ten_percent
+        new_stock_data.append(round(stock_sum))
+    return new_stock_data
 
 
-sales_columns = get_last_5_entries_sales()
 
 
 def main():
@@ -113,6 +126,18 @@ def main():
     update_worksheet(sales_data, 'sales')
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, 'surplus')
-    
-# main()
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, 'stock')
+   
+
+
+print("Welcom to Love Sandwiches Data Automation/n")    
+main()
+
+
+
+
+
+
 
